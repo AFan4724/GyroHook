2395b05e3404a101c1f54b71b80d9ad935b47ba2
AFan4724 <127580359+AFan4724@users.noreply.github.com>
2026-07-25 08:18:56 +0800

    chore: bump version to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "com.example.gyrohook"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
